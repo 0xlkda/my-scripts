c747c41bcfae5ab46880551d594a75f569e8ed10
0xlkda <lekhacduyanh@gmail.com>
2023-04-14 21:24:32 +0700

    more useful

--- harpsichordwithhammers.com.scripts.js
+++ harpsichordwithhammers.com.scripts.js
@@ -1,14 +1,45 @@
-var not = (myChoice) => i => i.textContent !== myChoice
-var getParent = i => i.parentElement
-var hide = i => i.style.display = 'none'
-var show = i => i.style = null
+var uniq = list => Array.from(new Set(list));
+var not = (query) => i => i.textContent !== query;
+var notSubject = i => i !== 'Subject';
+var getParent = i => i.parentElement;
+var hide = i => { i.style.display = 'none'; return i };
+var show = i => { i.style = null; return i };
 
-var showOnly = (category) => {
-  $x('//tr').forEach(show)
-  $x('//tr/td[1]')
-  .filter(not(category))
-  .map(getParent)
-  .forEach(hide)
+// create buttons for filtering
+document.querySelector('.entry-content .wp-block-table').prepend(...getSubjects());
+
+function getRows() {
+  return Array.from(document.querySelectorAll('.entry-content .wp-block-table tr'));
 }
 
-showOnly('Chords')
+function getFirstCol(row) {
+  return row.querySelector('td');
+}
+
+function showOnly(query) {
+  let rows = getRows().map(show);
+
+  if (query === 'All') {
+    return rows;
+  }
+
+  return rows.map(getFirstCol)
+    .filter(not(query))
+    .map(getParent)
+    .forEach(hide);
+}
+
+function getSubjects() {
+  let subjects = uniq(getRows().map(getFirstCol).map(x => x.textContent));
+  let triggers = subjects.map(createTrigger);
+  return triggers;
+}
+
+function createTrigger(subject) {
+  let query = subject === 'Subject' ? 'All' : subject;
+  let trigger = document.createElement('button');
+  trigger.name = query;
+  trigger.textContent = query;
+  trigger.onclick = () => showOnly(query);
+  return trigger;
+}
